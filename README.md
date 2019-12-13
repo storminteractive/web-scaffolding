3db8cb6212d0ee9b7c2b936b23d6dfba40466182
Paul <paul@storm-interactive.com>
2019-12-13 15:50:27 +0400

    version 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is going to be a node.js express scaffolding project
+And this is an update to it
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+const express = require('express');
+const path = require('path');
+const l = require('./libs/winston')('scaffolding-index');
+const ew = require('./libs/express-winston');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(ew);
+
+app.use(express.static(path.join(__dirname, '/public/')));
+//app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+
+app.get('/hello',(req,res) => {
+    // req.query - array of url?param1&param2, req.params - array of /hello/:name
+    if(req.query.name === undefined){
+        l.error("Our friend did not introduce themselves, reponding default");
+        res.send("Hello stranger");
+    } else {
+        l.info(`Got a request from a fellow called ${req.query.name}`);
+        res.send(`Hello there, ${req.query.name}`);
+    }
+});
+
+app.listen(port, () => {
+    l.info(`listening on port ${port}`);
+}); 
